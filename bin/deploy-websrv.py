00a5d1544ca3c76b6365e36a2da0c373fe02f134
William Strecker-Kellogg <willsk@bnl.gov>
2020-12-18 19:17:08 -0500

    Web hook working

--- bin/deploy-websrv.py
+++ bin/deploy-websrv.py
@@ -9,25 +9,39 @@ log = logging.getLogger(__name__)
 
 class WebhookHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    error_content_type = 'text/plain;charset=utf-8'
+    # Default error sends HTML
+    def send_error(self, code, msg, explain=''):
+        self.send_response(code, msg)
+        self.send_header('Connection', 'close')
+        self.send_header("Content-Type", 'text/plain;charset=utf8')
+        self.send_header("Content-Length", str(len(explain)))
+        self.end_headers()
+        if explain:
+            self.wfile.write(explain.encode('utf8'))
 
     def do_POST(self):
-        webdata = self.rfile.read()
-        log.info("WD: %s\nH: %s", webdata, self.headers)
+        postlen = int(self.headers['content-length'])
+        webdata = self.rfile.read(postlen)
         sig = self.headers['X-Gitea-Signature']
         if not sig:
             self.send_error(401, 'No signature provided')
             return
-        data = WebhookOutput(webdata, sig)
+        try:
+            self.process_data(webdata, sig)
+        except Exception as e:
+            self.send_error(500, 'Error processing request', str(e) + '\n')
+
+    def process_data(self, json, signature):
+        data = WebhookOutput(json, signature)
         if not data.validate():
             self.send_error(403, 'Invalid signature or repo')
             return
         response, ok = data.notify_daemon()
         if not ok:
-            self.send_error(500, response)
+            self.send_error(500, 'Error processing repo', response)
         else:
             self.end_headers()
-            self.wfile(response)
+            self.wfile.write(response)
 
 
 if __name__ == "__main__":
